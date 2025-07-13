c6551659bf0a689ef1b6022f50e40e9c99bb2791
Elizaveta-2025 <grgmiheev@gmail.com>
2025-07-13 20:21:25 +0300

    продолжаем делать 3.5

--- scripts/script.js
+++ scripts/script.js
@@ -61,5 +61,73 @@ window.addEventListener("DOMContentLoaded", () => {
             item.textContent = dataTitleGids[index];
         });
     }
-    
 });
+
+const headerMenu = document.querySelector('.main-nav');
+if (headerMenu){
+    const headerList = headerMenu.querySelector('.header_list');
+    const menuData = {
+        link1: {
+            link: 'index.html',
+            title: 'Главная',
+        },
+        link2: {
+            link: 'excursions.html',
+            title: 'Экскурсии',
+        },
+         link3: {
+            link: '#',
+            title: 'Личный кабинет',
+        }
+    }
+
+
+
+/*Задание 3.5
+    const cardsPrice = document.querySelector('.price');
+if (cardsPrice) {
+    const priceList = cardsPrice.querySelector('.price_list');
+
+    const cardsPriceData = {
+        price1: {
+            level: 'Социальный',
+            price: '25000 р.',
+            description: 'Скидки',
+            button: 'Оставить заявку'
+        },
+        price2: {
+            level: 'Классический',
+            price: '35000 р.',
+            description: 'Sale',
+            button: 'Оставить заявку'
+        },
+        price3: {
+            level: 'Студенческий',
+            price: '30000 р.',
+            description: 'Sale',
+            button: 'Оставить заявку'
+        }
+    }
+    const createCard = (level, price, description, button) => {
+        const card=
+       <li class="price_item">
+            <p class="price_level">${level}</p>
+            <p class="price_price">${price}</p>
+            <p class="price_description">${description}</p>
+            <button class="price_button button">${button}</button>
+        </li>
+        
+             
+        return card;
+    }
+
+        for (const cardKey in cardsPriceData) {
+            const card = cardsPriceData[cardKey];
+            const cardElement = createCard(card.level, card.price, card.description, card.button);
+            priceList.insertAdjacentHTML('beforeend', cardElement);
+        }
+}*/
+    
+
+
+
