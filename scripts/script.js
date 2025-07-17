dc669c88828a8d5187d817db18224bcd11b70747
Elizaveta-2025 <grgmiheev@gmail.com>
2025-07-17 17:46:15 +0300

    готовое задание 3.7

--- scripts/script.js
+++ scripts/script.js
@@ -201,5 +201,24 @@ preloader.remove();
 }
 
 
+// объявляем переменную sliders,куда помещаем элемент с классом swiper
+const sliders = document.querySelector('.swiper');
+//проверяем существует ли элемент
+    if (sliders) {
+        const swiper1 = new Swiper(sliders, {
+            // Пагинация
+            pagination: {
+                el: '.swiper-pagination',
+type: "fraction",
+            },
+
+                     // Навигационные стрелки
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }   
+
 
 })
